674b3381a70364b9f7ae9c968f143aeecaeacbe5
Gamesrack565 <angelhac17@gmail.com>
2025-11-04 14:45:03 -0600

    Version 2.1.1 - Actualizacion de titulo

--- Codigo/practica4_BCHL.py
+++ Codigo/practica4_BCHL.py
@@ -10,15 +10,16 @@ from Servicios.base_Datos import create_db_and_tables
 from Rutas import categorias, items, envios, optimizar
 
 app = FastAPI(
-    title="Práctica 4: Relaciones con Base de Datos",
+    title="Práctica 4: Relaciones con Base de Datos (Ordenado)",
     description="API para gestionar Items, Categorías y Envíos, con persistencia de datos."
+    version="2.1.1",
 )
 
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
 
-# Registrar rutas
+#Registrar rutas
 app.include_router(categorias.router)
 app.include_router(items.router)
 app.include_router(envios.router)
